f2daf6e22e592153f542d42105bb5bd76311680c
Neeraj Singh <linux023582@gmail.com>
2026-07-03 16:28:41 +0530

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let ans = []
+        ans.push(nums,nums)
+        return ans
+
+
+    }
+}
