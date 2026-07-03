78ae76c7414511eeafcf50b63c68d6ebfbcb4043
Neeraj Singh <linux023582@gmail.com>
2026-07-03 20:57:36 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.js
@@ -0,0 +1,28 @@
+class Solution {
+    isAnagram(s, t) {
+        if(s.length != t.length){
+            return false
+        }
+        let newS = {}
+        for(let str of s){
+            if(str in newS){
+                newS[str] += 1
+            }else{
+                newS[str] = 1
+            }
+        }
+        for(let str of t){
+            if(str in newS){
+                newS[str] -= 1
+            }
+        }
+        for(let key in newS){
+            if(newS.hasOwnProperty(key)){
+                if(newS[key] > 0){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
